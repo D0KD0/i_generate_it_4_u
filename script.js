4240eb2cc7e5fadc8756ceec3d7b4a6a98386f7e
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2022-10-02 23:12:46 -0700

    code updated

--- script.js
+++ script.js
@@ -20,93 +20,76 @@ generateBtn.addEventListener("click", writePassword);
 
 // Prompts that come up after you click generate password
 function generatePassword() {
-  while (true) {
-    let passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
-      if (passwordLength >=8 && passwordLength <= 128) {
-        break;
-      } else {
-        prompt("Please enter the number of characters between 8 and 128");
-      }
-  }
 
-  while (true) {
-    let numbers = prompt("Do you want numbers? (y/n)");
-      if (numbers === "y") {
-        numbers = "1234567890";
-        alert("Numbers will be included");
-        break;
-      } else if (numbers === "n") {
-        numbers = "";
-        alert("Numbers will not be included");
-        break;
-      } else {
-        prompt("Do you want numbers? (y/n)");
-      }
-  }
+  let passwordLength = prompt("Please enter the number of characters between 8 and 128");
+   if (passwordLength >= 8 && passwordLength <= 128) {
+      alert("Character length has been selected");
+    } else if (passwordLength < 8 || passwordLength >128) {
+      alert("This is out of range. Number of characters should be between 8 and 128.");
+    }
 
-  while (true) { 
+  let numbers = prompt("Do you want numbers? (y/n)");
+    if (numbers === "y") {
+      numbers = "1234567890";
+      alert("Numbers will be included");
+    } else if (numbers === "n") {
+      numbers = "";
+      alert("Numbers will not be included");
+    } else if (numbers !== "y"||"n"){
+      alert("This is wrong selection. Enter y or n", "y/n");
+      prompt("Do you want numbers? (y/n)");
+    }
+    
   let lowerCases = prompt("Do you want lower cases? (y/n)");
     if (lowerCases === "y") {
       lowerCases = "abcdefghijklmnopqrstuvwxyz";
       alert("lowerCases will be included");
-      break;
     } else if (lowerCases === "n") {
       lowerCases = "";
       alert("lowerCases will not be included");
-      break;
     } else if (lowerCases !== "y"||"n"){
       alert("This is wrong selection. Enter y or n", "y/n");
     }
-  }
 
-  while (true) {
   let upperCases = prompt("Do you want uppercases? (y/n)");
     if (upperCases === "y") {
       upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXY";
       alert("Uppercases will be included");
-      break;
     } else if (upperCases === "n") {
       upperCases = "";
       alert("Uppercases will not be included");
-      break;
     } else if (upperCases !== "y"||"n"){
       alert("This is wrong selection. Enter y or n", "y/n");
     }
-  }
 
-  while(true) {
   let specialCharacters = prompt("Do you want special characters? (y/n)");
     if (specialCharacters === "y") {
       specialCharacters = " !#$%&'()*+,-./:;<=>?@]/^_`{|}~";
       alert("Special characters will be included");
-      break;
     } else if (specialCharacters === "n") {
       specialCharacters = "";
       alert("Special characters will not be included");
-      break;
     } else if (specialCharacters !== "y"||"n") {
-      alert("This is wrong selection. Enter y or n", "y/n");
+      alert("This is wrong selection. Enter y or n", "y/n")
     }
-  }
 
-  // To verify values are correctly selected --
-  //console.log(passwordLength, numbers, lowerCases, upperCases, specialCharacters);
+// To verify values are correctly selected --
+//  console.log(passwordLength, numbers, lowerCases, upperCases, specialCharacters);
 
   var totalCharacters = ''.concat(numbers, lowerCases, upperCases, specialCharacters);
 
-  // to make sure characters are correctly selected --
-  console.log(totalCharacters);
+// to make sure characters are correctly selected --
+// console.log(totalCharacters);
 
-  // generate random password
+// generate random password
 
   var result = "";
 
   for (var i = 0; i < passwordLength; i++) {
-      var randomPass = Math.floor(Math.random() * totalCharacters.length);
-      result += totalCharacters.substring(randomPass, randomPass+1);
-  }
-  
-  return result;
+    var randomPass = Math.floor(Math.random() * totalCharacters.length);
+    result += totalCharacters.substring(randomPass, randomPass+1);
+ }
+ 
+ return result;
 
-} 
-
+}
